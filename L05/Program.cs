38f06c739e8ecfdfff0f886d538e15a2dbf46d74
MARKUS\Markus Schnee <markus.schnee98@gmx.de>
2018-11-13 20:58:56 +0100

    test

---
+++ L05/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            
+            //turnaroundword();
+            turnaroundsentence();
+        }
+
+        public static void turnaroundword()
+        {
+            Console.WriteLine("Geben Sie das zu umdrehende Wort an: ");
+            string inputWord = Console.ReadLine();
+            char[] word = inputWord.ToCharArray();
+            Array.Reverse(word);
+
+            Console.WriteLine("So lautet ihr Wort umgedreht: ");
+            Console.WriteLine(word);
+        }
+
+        public static void turnaroundsentence()
+        {
+
+            Console.WriteLine("Geben Sie einen Satz an: ");
+            string inputSatz = Console.ReadLine();
+            string[] zerleger = inputSatz.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine("Ihr Satz wird von hinten nach vorne zurückgegeben: ");
+
+            for (int i = zerleger.Length - 1; i >= 0; i--)
+            {
+               
+                Console.WriteLine(zerleger[i]);
+            }
+
+
+        }
+
+
+    }
+}
